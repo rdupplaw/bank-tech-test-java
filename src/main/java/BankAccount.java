1ec3d09c5d810743a75e95c37dd956e5891c0de8
Robert Dupplaw <dupplawrobert@gmail.com>
2021-08-27 15:28:30 +0100

    Simplify previous and new balance calculation

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -2,31 +2,43 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class BankAccount {
-    private final ArrayList<BankTransaction> transactions = new ArrayList<>();
+    private final ArrayList<BankTransaction> transactions;
     private final BankAccountStatementGenerator bankAccountStatementGenerator;
-    private int previousBalance = 0;
 
     public BankAccount(BankAccountStatementGenerator bankAccountStatementGenerator) {
+        this.transactions = new ArrayList<>();
         this.bankAccountStatementGenerator = bankAccountStatementGenerator;
     }
 
     public void deposit(int amount, LocalDate date) {
-        BankTransaction transaction = BankTransaction.Builder.amount(amount).date(date).balance(previousBalance + amount).build();
-        this.previousBalance = previousBalance + amount;
-        transactions.add(transaction);
+        transactions.add(new BankTransaction(amount, date, getNewBalance(amount, BankTransactionType.CREDIT), BankTransactionType.CREDIT));
     }
 
     public void withdraw(int amount, LocalDate date) {
-        if (previousBalance - amount < 0) {
+        if (getNewBalance(amount, BankTransactionType.DEBIT) < 0) {
             throw new ArithmeticException("Cannot withdraw below zero");
         }
 
-        BankTransaction transaction = BankTransaction.Builder.amount(-amount).date(date).balance(previousBalance - amount).build();
-        this.previousBalance = previousBalance - amount;
-        transactions.add(transaction);
+        transactions.add(new BankTransaction(-amount, date, getNewBalance(amount, BankTransactionType.DEBIT), BankTransactionType.DEBIT));
     }
 
     public String generateStatement() {
         return this.bankAccountStatementGenerator.generateStatement(transactions);
+    }
+
+    private int getNewBalance(int amount, BankTransactionType type) {
+        return getPreviousBalance() + switch (type) {
+            case DEBIT -> -amount;
+            case CREDIT -> amount;
+        };
+    }
+
+    private int getPreviousBalance() {
+        if (transactions.isEmpty()) {
+            return 0;
+        }
+
+        BankTransaction lastTransaction = transactions.get(transactions.size() - 1);
+        return lastTransaction.balance();
     }
 }
