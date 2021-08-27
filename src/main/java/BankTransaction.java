e67a3fe98cf409cf0b1b3c3b04445d503306a940
Robert Dupplaw <dupplawrobert@gmail.com>
2021-08-27 15:27:54 +0100

    Remove BankTransaction builder class

--- src/main/java/BankTransaction.java
+++ src/main/java/BankTransaction.java
@@ -1,31 +1,3 @@
 import java.time.LocalDate;
 
-record BankTransaction(int amount, LocalDate date, int balance) {
-    public static class Builder {
-        private int amount;
-        private LocalDate date;
-        private int balance;
-
-        private Builder(int amount) {
-            this.amount = amount;
-        }
-
-        public static Builder amount(int amount) {
-            return new Builder(amount);
-        }
-
-        public Builder date(LocalDate date) {
-            this.date = date;
-            return this;
-        }
-
-        public Builder balance(int balance) {
-            this.balance = balance;
-            return this;
-        }
-
-        public BankTransaction build() {
-            return new BankTransaction(amount, date, balance);
-        }
-    }
-}
+record BankTransaction(int amount, LocalDate date, int balance, BankTransactionType type) {}
